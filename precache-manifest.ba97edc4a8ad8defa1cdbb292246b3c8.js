8d06894fcf2e0ab47ad31c02e9ed0031034aa40c
ting-thaiguy <plaimanus.lueondee@temeda.com>
2019-03-11 23:36:28 +0700

    Updates

---
+++ precache-manifest.ba97edc4a8ad8defa1cdbb292246b3c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/payatao/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "52c000f04739e6af81a4",
+    "url": "/payatao/static/js/runtime~main.373bcf7c.js"
+  },
+  {
+    "revision": "9d3a39a5580a3c2a0c5b",
+    "url": "/payatao/static/js/main.3b3d810a.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/payatao/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "9d3a39a5580a3c2a0c5b",
+    "url": "/payatao/static/css/main.5dd7b3d1.chunk.css"
+  },
+  {
+    "revision": "19d099c43267af7ff20f135c9fece8ee",
+    "url": "/payatao/index.html"
+  }
+];
